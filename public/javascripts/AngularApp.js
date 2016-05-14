aeadde36d422a2560bac4a7ca4b75844291368b0
Avash <avashpdl@yahoo.com>
2016-05-14 17:11:07 +0545

    Add post and comment succcessfully done

--- public/javascripts/AngularApp.js
+++ public/javascripts/AngularApp.js
@@ -1,3 +1,5 @@
+
+
 var app = angular.module('flapperNews', ['ui.router']);
 
 app.controller('MainCtrl', [
@@ -18,13 +20,27 @@ $scope.posts = posts.posts;
 
 $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({
-    title: $scope.title,
-    link: $scope.link,
-    upvotes: 0
-  });
+	$scope.posts.push({
+	title: $scope.title,
+	link: $scope.link,
+	upvotes: 0,
+	comments: [
+	{author: 'Joe', body: 'Cool post!', upvotes: 0},
+	{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+	]
+	});
   $scope.title = '';
   $scope.link = '';
+};
+
+$scope.addComment = function(){
+  if($scope.body === '') { return; }
+  $scope.post.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
 };
 
 
@@ -72,4 +88,23 @@ app.controller('PostsCtrl', [
 'posts',
 function($scope, $stateParams, posts){
 
+
+	
+
+
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function(){
+  if($scope.body === '') { return; }
+  $scope.post.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
+};
+
 }]);
+
+
+
